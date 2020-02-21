c07b8ab88abb9cd7cf90e9fc02cc72af65a3ec51
Colin Millar <colin.millar@ices.dk>
2020-02-21 15:43:24 +0100

    Update data.R

--- data.R
+++ data.R
@@ -18,7 +18,7 @@ recssb <-
   data.frame(
     recruitment = summary_tab$recruitment[-seq_len(rec_age)],
     yearclass = summary_tab$Year[-seq_len(rec_age)] - rec_age,
-    ssb = summary_tab$SSB[seq_len(nrow(summary_tab) - rec_age)]
+    ssb = summary_tab$SSB[seq_len(nrow(summary_tab) - rec_age) + 1]
   )
 
 # write out for use in model script
